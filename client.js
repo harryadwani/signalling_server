5d0c78a9e63cb5b66a395f6b61ccb70903240414
harryadwani <52936040+harryadwani@users.noreply.github.com>
2020-06-07 16:40:37 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-var connection = new WebSocket('ws://localhost:9090'); 
+var connection = new WebSocket('ws://localhost:8080'); 
 var name = ""; 
  
 var loginInput = document.querySelector('#loginInput'); 
